46d545a1c6e4814dc0b2f56720ab6977f26c7871
CBuPenblu <ikulagins@outlook.com>
2024-08-02 02:16:56 +0300

    video1
    Constructing different types of charts

---
+++ video1_Scatter_diagram.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 4, 6, 7]
+y = [3, 5, 8, 10]
+
+plt.scatter(x, y)
+
+plt.xlabel("axel X")
+plt.ylabel("axel Y")
+plt.title("Test scatter diagram")
+
+plt.show()
